fda5b441db8cc669ba00ff571a79af5154ba9ec1
NatalieLouis <781798334@qq.com>
2025-01-23 11:14:46 -0500

    without atomic is dead while

--- main.cpp
+++ main.cpp
@@ -2,22 +2,24 @@
 #include <iostream>
 #include <thread>
 
-std::atomic<int> counter(0);
+std::atomic<int> shared_var(0);
 
-void increment() {
-  for (int i = 0; i < 10000; ++i) {
-    counter.fetch_add(1, std::memory_order_relaxed);
+void writer() {
+  shared_var.store(1, std::memory_order_release); // 写操作，释放内存序
+}
+
+void reader() {
+  while (!shared_var.load(std::memory_order_acquire)) { // 读操作，获取内存序
+    // 等待 shared_var 变为 1
   }
+  std::cout << "Reader sees shared_var as 1" << std::endl;
 }
 
 int main() {
-  std::thread t1(increment);
-  std::thread t2(increment);
+  std::thread t1(writer);
+  std::thread t2(reader);
 
   t1.join();
   t2.join();
-
-  std::cout << "Final counter value: "
-            << counter.load(std::memory_order_relaxed) << std::endl;
   return 0;
 }
