9b8947b73720323bc5f8cda23527e90d83ce3671
NatalieLouis <781798334@qq.com>
2025-02-22 05:33:21 -0500

    偏特化

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
-template <bool B, class T = void>
-struct enable_if;
+#include <iostream>
+#include <string>
 
-template <class T>
-struct enable_if<true, T> {
-    typedef T type;
+// 通用 Pair 类模板
+template <typename T, typename U>
+class Pair {
+public:
+    T first;
+    U second;
+
+    Pair(T a, U b)
+        : first(a)
+        , second(b)
+    {
+    }
+
+    void print() const
+    {
+        std::cout << "Pair: " << first << ", " << second << std::endl;
+    }
 };
 
-template <typename T>
-std::enable_if_t<条件, 返回类型> 函数名(参数列表);
+// 类模板偏特化：当第二个类型是指针时
+template <typename T, typename U>
+class Pair<T, U*> {
+public:
+    T first;
+    U* second;
+
+    Pair(T a, U* b)
+        : first(a)
+        , second(b)
+    {
+    }
+
+    void print() const
+    {
+        std::cout << "Pair with pointer: " << first << ", " << *second << std::endl;
+    }
+};
+
+int main()
+{
+    Pair<int, double> p1(1, 2.5);
+    p1.print(); // 输出：Pair: 1, 2.5
+
+    double value = 3.14;
+    Pair<std::string, double*> p2("Pi", &value);
+    p2.print(); // 输出：Pair with pointer: Pi, 3.14
+
+    return 0;
+}
