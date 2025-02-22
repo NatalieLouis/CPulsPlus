90fae4f317a63a52ef634265010aa046e8d39769
NatalieLouis <781798334@qq.com>
2025-02-22 09:49:28 -0500

    false_type and declval

--- main.cpp
+++ main.cpp
@@ -1,48 +1,26 @@
 #include <iostream>
 #include <type_traits>
+#include <utility> // 包含 std::declval
 
-// 辅助类型，检测是否存在成员函数 foo
+// 原始模板定义
+template <typename T, typename = void>
+struct HasFoo : std::false_type { };
+
+// 偏特化：检查类型 T 是否有成员函数 foo()
 template <typename T>
-class has_foo {
-private:
-    typedef char yes[1];
-    typedef char no[2];
+struct HasFoo<T, std::void_t<decltype(std::declval<T>().foo())>> : std::true_type { };
 
-    template <typename U, void (U::*)()>
-    struct SFINAE { };
-
-    template <typename U>
-    static yes& test(SFINAE<U, &U::foo>*);
-
-    template <typename U>
-    static no& test(...);
-
-public:
-    static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes);
+// 测试类
+struct A {
+    int foo(int num) { return num; }
 };
 
-// 函数仅在 T 有 foo() 成员时启用
-template <typename T>
-typename std::enable_if<has_foo<T>::value, void>::type
-call_foo(T& obj)
-{
-    obj.foo();
-    std::cout << "foo() called." << std::endl;
-}
-
-class WithFoo {
-public:
-    void foo() { std::cout << "WithFoo::foo()" << std::endl; }
-};
-
-class WithoutFoo { };
+struct B { };
 
 int main()
 {
-    WithFoo wf;
-    call_foo(wf); // 输出: WithFoo::foo() \n foo() called.
-
-    // WithoutFoo wf2;
-    // call_foo(wf2); // 编译错误，没有匹配的函数
+    std::cout << std::boolalpha;
+    std::cout << "HasFoo<A>::value: " << HasFoo<A>::value << std::endl; // 输出：true
+    std::cout << "HasFoo<B>::value: " << HasFoo<B>::value << std::endl; // 输出：false
     return 0;
 }
