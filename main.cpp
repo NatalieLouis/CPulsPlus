049067d3ab524c59474c442c0770b3093478ffa6
NatalieLouis <781798334@qq.com>
2025-02-22 04:33:11 -0500

    c++20

--- main.cpp
+++ main.cpp
@@ -1,41 +1,17 @@
+#include <concepts>
 #include <iostream>
-#include <type_traits>
 
-// 辅助类型，检测是否存在成员函数 foo
-template <typename T> class has_foo {
-private:
-  typedef char yes[1];
-  typedef char no[2];
-
-  template <typename U, void (U::*)()> struct SFINAE {};
-
-  template <typename U> static yes &test(SFINAE<U, &U::foo> *);
-
-  template <typename U> static no &test(...);
-
-public:
-  static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes);
-};
-
-// 函数仅在 T 有 foo() 成员时启用
-template <typename T>
-typename std::enable_if<has_foo<T>::value, void>::type call_foo(T &obj) {
-  obj.foo();
-  std::cout << "foo() called." << std::endl;
+template <std::integral T> void print(T value) {
+  std::cout << "Integral value: " << value << std::endl;
 }
 
-class WithFoo {
-public:
-  void foo() { std::cout << "WithFoo::foo()" << std::endl; }
-};
-
-class WithoutFoo {};
+template <typename T>
+  requires(!std::integral<T>)
+void print(T value) {
+  std::cout << "Non-integral value: " << value << std::endl;
+}
 
 int main() {
-  WithFoo wf;
-  call_foo(wf); // 输出: WithFoo::foo() \n foo() called.
-
-  // WithoutFoo wf2;
-  // call_foo(wf2); // 编译错误，没有匹配的函数
-  return 0;
+  print(42);   // 输出：Integral value: 42
+  print(3.14); // 输出：Non-integral value: 3.14
 }
