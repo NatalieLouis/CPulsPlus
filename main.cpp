f758a20ef9f601b21cd464dd33aa01ee6958083e
NatalieLouis <781798334@qq.com>
2025-02-22 10:38:26 -0500

    比较

--- main.cpp
+++ main.cpp
@@ -6,21 +6,53 @@
 template <typename T, typename = void>
 struct HasFoo : std::false_type { };
 
-// 偏特化：检查类型 T 是否有成员函数 foo()
+// 偏特化：检查类型 T 是否有成员函数 int foo(int)
 template <typename T>
-struct HasFoo<T, std::void_t<decltype(std::declval<T>().foo())>> : std::true_type { };
+struct HasFoo<T, std::void_t<decltype(std::declval<T>().foo(std::declval<int>()))>> {
+    static constexpr bool value = std::is_same<decltype(std::declval<T>().foo(std::declval<int>())), int>::value;
+};
 
 // 测试类
 struct A {
-    int foo(int num) { return num; }
+    int foo(int num) { return num; } // 匹配 int foo(int)
 };
 
-struct B { };
+struct B {
+    void foo(int num) { } // 不匹配，返回类型不是 int
+};
+
+struct C {
+    int foo(double num) { return static_cast<int>(num); } // 不匹配，参数类型不是 int
+};
+
+struct D {
+    int foo() { return 42; } // 不匹配，参数数量不一致
+};
+
+// 辅助函数，用于检查参数类型是否严格为 int
+template <typename T>
+constexpr auto check_foo(int) -> decltype(std::declval<T>().foo(std::declval<int>()), std::true_type {})
+{
+    return {};
+}
+
+template <typename T>
+constexpr std::false_type check_foo(...);
+
+template <typename T>
+struct HasFooStrict : decltype(check_foo<T>(0)) { };
 
 int main()
 {
     std::cout << std::boolalpha;
     std::cout << "HasFoo<A>::value: " << HasFoo<A>::value << std::endl; // 输出：true
     std::cout << "HasFoo<B>::value: " << HasFoo<B>::value << std::endl; // 输出：false
+    std::cout << "HasFoo<C>::value: " << HasFoo<C>::value << std::endl; // 输出：true
+    std::cout << "HasFoo<D>::value: " << HasFoo<D>::value << std::endl; // 输出：false
+
+    std::cout << "HasFooStrict<A>::value: " << HasFooStrict<A>::value << std::endl; // 输出：true
+    std::cout << "HasFooStrict<B>::value: " << HasFooStrict<B>::value << std::endl; // 输出：true
+    std::cout << "HasFooStrict<C>::value: " << HasFooStrict<C>::value << std::endl; // 输出：true
+    std::cout << "HasFooStrict<D>::value: " << HasFooStrict<D>::value << std::endl; // 输出：false
     return 0;
 }
