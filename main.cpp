755c0c8663cb6334c533e477475830f0ee073f9b
NatalieLouis <781798334@qq.com>
2025-02-22 11:02:43 -0500

    enable_if_t

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <type_traits>
 
-template <typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
-void bar(T x)
+template <typename T>
+typename std::enable_if_t<std::is_integral<T>::value, int>
+baz(T x)
 {
     std::cout << "Integral: " << x << std::endl;
+    return x;
 }
 
 int main()
 {
-    bar(10); // 正常工作
-    // bar(3.14); // 编译错误，因为 T 不是整型
+    std::cout << baz(10); // 正常工作
+    // baz(3.14); // 编译错误，因为 T 不是整型
     return 0;
 }
